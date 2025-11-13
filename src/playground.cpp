a128b6dab850656bab50c257b4a87d5804d4dc2a
lym01803 <998244353@sjtu.edu.cn>
2025-11-13 17:05:45 +0800

    [test] test ranges efficiency

--- src/playground.cpp
+++ src/playground.cpp
@@ -13,23 +13,28 @@ void get_right_triangle_triples() {
     const auto for_each = []<typename F>(F&& f) {
         return std::views::transform(std::forward<F>(f)) | std::views::join;
     };
+    long long counter {0LL};
     auto seq = std::views::iota(1LL) 
-        | for_each([&for_each](const long long c){
+        | for_each([&for_each, &counter](const long long c){
             return std::views::iota(1LL, c) 
-                | for_each([=, &for_each](const long long a) {
+                | for_each([=, &for_each, &counter](const long long a) {
                     return std::views::iota(a, c) 
-                        | for_each([=, &for_each](const long long b) {
+                        | for_each([=, &for_each, &counter](const long long b) {
+                            counter += 1;
                             return a * a + b * b == c * c
                                 ? optional_view {std::tuple {a, b, c}}
                                 : optional_view<decltype(std::tuple {a, b, c})> {};
                         });
                 });
         });
-    for (const auto & item : seq | std::views::take(10)) {
-        std::cout << std::get<0>(item) << " "
-            << std::get<1>(item) << " "
-            << std::get<2>(item) << std::endl;
+    long long s {0LL};
+    for (const auto & item : seq | std::views::take(1000)) {
+        // std::cout << std::get<0>(item) << " "
+        //     << std::get<1>(item) << " "
+        //     << std::get<2>(item) << std::endl;
+        s += std::get<2>(item);
     }
+    std::cout << std::format("counter = {}, s = {}", counter, s) << std::endl;
 }
 
 } // namespace playground
