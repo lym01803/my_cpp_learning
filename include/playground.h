d64ed51f2ff8c70e2a35692820ab9239924fe26b
lym01803 <998244353@sjtu.edu.cn>
2025-11-20 17:55:28 +0800

    [add] clang-format google style

--- include/playground.h
+++ include/playground.h
@@ -5,57 +5,55 @@
 #include <concepts>
 #include <exception>
 #include <functional>
+#include <iostream>
 #include <mutex>
-#include <vector>
 #include <optional>
 #include <ranges>
-#include <iostream>
 #include <thread>
+#include <vector>
 
 namespace playground {
 
 template <std::semiregular T>
-struct optional_view: public std::ranges::view_interface<optional_view<T> > {
-    optional_view() = default;
-    optional_view(T data): data(std::move(data)) {}
+struct optional_view : public std::ranges::view_interface<optional_view<T>> {
+  optional_view() = default;
+  optional_view(T data) : data(std::move(data)) {}
 
-    T const * begin() const noexcept {
-        return data ? &data.value() : nullptr;
-    }
-    T const * end() const noexcept {
-        return data ? &data.value() + 1 : nullptr;
-    }
-private:
-    std::optional<T> data{};
+  T const* begin() const noexcept { return data ? &data.value() : nullptr; }
+  T const* end() const noexcept { return data ? &data.value() + 1 : nullptr; }
+
+ private:
+  std::optional<T> data{};
 };
 
 inline auto get_right_triangle_triples(int n) {
-    const auto for_each = []<typename F>(F&& f) {
-        return std::views::transform(std::forward<F>(f)) | std::views::join;
-    };
-    auto seq = std::views::iota(1LL) 
-        | for_each([&for_each](const long long c){
-            return std::views::iota(1LL, c) 
-                | for_each([=, &for_each](const long long a) {
-                    return std::views::iota(a, c) 
-                        | for_each([=, &for_each](const long long b) {
-                            return a * a + b * b == c * c
-                                ? optional_view {std::tuple {a, b, c}}
-                                : optional_view<decltype(std::tuple {a, b, c})> {};
-                        });
-                });
-        });
-    return seq | std::views::take(n);
+  const auto for_each = []<typename F>(F&& f) {
+    return std::views::transform(std::forward<F>(f)) | std::views::join;
+  };
+  auto seq = std::views::iota(1LL) | for_each([&for_each](const long long c) {
+               return std::views::iota(1LL, c) |
+                      for_each([=, &for_each](const long long a) {
+                        return std::views::iota(a, c) |
+                               for_each([=, &for_each](const long long b) {
+                                 return a * a + b * b == c * c
+                                            ? optional_view{std::tuple{a, b, c}}
+                                            : optional_view<decltype(std::tuple{
+                                                  a, b, c})>{};
+                               });
+                      });
+             });
+  return seq | std::views::take(n);
 }
 
 inline void ranges_sort(int n) {
-    std::vector vec {std::from_range, get_right_triangle_triples(n)};
-    std::ranges::sort(vec, std::ranges::less {}, [](const auto &t) { return std::get<0>(t); });
-    for (const auto &t : vec) {
-        std::cout << std::get<0>(t) << " " << std::get<1>(t) << " "
-            << std::get<2>(t) << std::endl;
-    }
-    std::cout << std::endl;
+  std::vector vec{std::from_range, get_right_triangle_triples(n)};
+  std::ranges::sort(vec, std::ranges::less{},
+                    [](const auto& t) { return std::get<0>(t); });
+  for (const auto& t : vec) {
+    std::cout << std::get<0>(t) << " " << std::get<1>(t) << " "
+              << std::get<2>(t) << std::endl;
+  }
+  std::cout << std::endl;
 }
 
 void try_concurrency();
@@ -63,55 +61,46 @@ void try_concurrency();
 void try_mutex();
 
 class guarded_thread {
-    std::thread t;
-public:
-    guarded_thread(std::thread t): t(std::move(t)) {}
-    guarded_thread(const guarded_thread &other) = delete;
-    guarded_thread(guarded_thread &&other) noexcept = default;
-    guarded_thread & operator = (const guarded_thread &other) = delete;
-    guarded_thread & operator = (guarded_thread &&other) noexcept {
-        if (t.joinable()) {
-            try {
-                t.join();
-            } catch (...) {
-                std::terminate();
-            }
-        }
-        t = std::move(other.t);
-        return *this;
-    }
+  std::thread t;
 
-    ~guarded_thread() {
-        if (t.joinable()) {
-            t.join();
-        }
+ public:
+  guarded_thread(std::thread t) : t(std::move(t)) {}
+  guarded_thread(const guarded_thread& other) = delete;
+  guarded_thread(guarded_thread&& other) noexcept = default;
+  guarded_thread& operator=(const guarded_thread& other) = delete;
+  guarded_thread& operator=(guarded_thread&& other) noexcept {
+    if (t.joinable()) {
+      try {
+        t.join();
+      } catch (...) {
+        std::terminate();
+      }
     }
+    t = std::move(other.t);
+    return *this;
+  }
 
-    std::thread & operator *() noexcept {
-        return t;
+  ~guarded_thread() {
+    if (t.joinable()) {
+      t.join();
     }
-    std::thread const & operator *() const noexcept {
-        return t;
-    }
-    std::thread * operator ->() noexcept {
-        return &t;
-    }
-    std::thread const * operator ->() const noexcept {
-        return &t;
-    }
-    std::thread release() noexcept {
-        return std::move(t);
-    }
+  }
+
+  std::thread& operator*() noexcept { return t; }
+  std::thread const& operator*() const noexcept { return t; }
+  std::thread* operator->() noexcept { return &t; }
+  std::thread const* operator->() const noexcept { return &t; }
+  std::thread release() noexcept { return std::move(t); }
 };
 
 template <std::invocable F>
-    requires std::move_constructible<F>
+  requires std::move_constructible<F>
 auto timer_wrap(F f) {
-    return [f = std::move(f)]() mutable {
-        const auto start_time = std::chrono::high_resolution_clock::now();
-        std::invoke(f);
-        return std::chrono::high_resolution_clock::now() - start_time;
-    };
+  return [f = std::move(f)]() mutable {
+    const auto start_time = std::chrono::high_resolution_clock::now();
+    std::invoke(f);
+    return std::chrono::high_resolution_clock::now() - start_time;
+  };
 }
 
-} // namespace playground
+}  // namespace playground
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
-#include "playground.h"
+#include <playground.h>
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
-    // playground::get_right_triangle_triples(10);
-    // playground::ranges_sort(10);
-    // playground::try_concurrency();
-    playground::try_mutex();
-    return 0;
+  std::cout << "Hello, world!" << std::endl;
+  // playground::get_right_triangle_triples(10);
+  // playground::ranges_sort(10);
+  // playground::try_concurrency();
+  playground::try_mutex();
+  return 0;
 }
